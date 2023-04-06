3b619f30707f85e6770d374ba627f1feeb1066ac
Gunwoo Kim <47517621+kgwoo@users.noreply.github.com>
2023-04-06 19:00:42 +0900

    [level 2] Title: H-Index, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+func solution(_ citations:[Int]) -> Int {
+    var h = 0
+    var hIndex:Int = 0
+    var isCalc = true
+    
+    
+    while isCalc {
+        var cnt = 0
+        h += 1
+        
+        for citation in citations{
+            if h <= citation {
+                cnt += 1
+            }
+        }
+        
+        if  cnt >= h {
+             hIndex = h
+        }else{
+            isCalc = false
+        }
+    }
+    
+    return hIndex
+}
